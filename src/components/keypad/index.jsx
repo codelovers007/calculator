935bd07eeb886465f74d39e7828b97fb3932a2ac
Niraj Dharwal <ndharwal624@gmail.com>
2022-11-09 16:16:26 +0530

    Refactor code

--- src/components/keypad/index.jsx
+++ src/components/keypad/index.jsx
@@ -16,14 +16,6 @@ class Keypad extends React.Component {
     }
   
     keypressHandler = ev => {
-      const {
-        setNumberValue,
-        setOperatorValue,
-        equalHandler,
-        allClear,
-        deleteChar,
-      } = this
-
       const numRegex = /^([0-9]|\.)*$/g;
       const opRegex =  /[+|\-|:|*]/g;
       const eqRegex =  /(=)/g;
@@ -38,23 +30,23 @@ class Keypad extends React.Component {
       const isAc = !!acRegex.exec(key)
       
       if (key && isNumber) {
-        setNumberValue(key + '')
+        this.setNumberValue(key + '')
       }
       
       if (key && isOperator) {
-        setOperatorValue(key + '')
+        this.setOperatorValue(key + '')
       }
       
       if (key && isEqual) {
-        equalHandler()
+        this.equalHandler()
       }
       
       if (key && isDel) {
-        deleteChar()
+        this.deleteChar()
       }
       
       if (key && isAc) {
-        allClear()
+        this.allClear()
       }
     }
   
@@ -80,50 +72,39 @@ class Keypad extends React.Component {
     }
     
     setNumberValue = value => {
-      const {
-          firstVal,
-          secondVal,
-          operator,
-        } = this.state
-      const { 
-        fixNumberString,
-        setDisplay,
-      } = this
       let total
-      
-      // if point is pressed, check if we already have it in current states
+
       if (value === '.') {
-          if (!operator && !this.hasPoint(firstVal)) {     	
-          total = fixNumberString(firstVal + value)
+          if (!this.state.operator && !this.hasPoint(this.state.firstVal)) {     	
+          total = this.fixNumberString(this.state.firstVal + value)
                   this.setState({
                      firstVal: total
               })
         }
-          if (!!operator && !this.hasPoint(secondVal)) {
-          total = fixNumberString(secondVal + value)
+          if (!!this.state.operator && !this.hasPoint(this.state.secondVal)) {
+          total = this.fixNumberString(this.state.secondVal + value)
                   this.setState({
                      secondVal: total
               })
         }
         if (total) {
-          setDisplay(total + '')
+          this.setDisplay(total + '')
         }
         return
       }
-      
-      // if input is a number, check if it's first or second number
-      if (!operator) {
-        total = fixNumberString(firstVal + value)
+
+      if (!this.state.operator) {
+        total = this.fixNumberString(this.state.firstVal + value)
         this.setState({
             firstVal: total
         })
       } else {
-        total = fixNumberString(secondVal + value)
+        total = this.fixNumberString(this.state.secondVal + value)
         this.setState({
             secondVal: total
         })
       }
-      setDisplay(total + '')
+      this.setDisplay(total + '')
     }
     
     setDisplay = value => {
@@ -133,12 +114,7 @@ class Keypad extends React.Component {
     }
     
     getCurrentTargetValue = () => {
-      const {
-        firstVal,
-        secondVal,
-        operator,
-      } = this.state
-      return !operator ? firstVal : secondVal
+      return !this.state.operator ? this.state.firstVal : this.state.secondVal
     }
     
     numberClickHandler = (e) => {
@@ -149,73 +125,51 @@ class Keypad extends React.Component {
     }
     
     setOperatorValue = operatorInput => {
-        const { 
-        firstVal,
-        secondVal,
-        operator,
-        display,
-      } = this.state
-
-      const { 
-        fixNumberString,
-        calculate,
-        setDisplay,
-      } = this
-
-      const fixedNumber = fixNumberString(firstVal, false)
+      const fixedNumber = this.fixNumberString(this.state.firstVal, false)
       
-      if (firstVal && !secondVal) {
+      if (this.state.firstVal && !this.state.secondVal) {
         this.setState({
           operator: operatorInput,
           display: fixedNumber,
         })
-      } else if (firstVal && operator && secondVal) {
-       const total = calculate()
+      } else if (this.state.firstVal && this.state.operator && this.state.secondVal) {
+       const total = this.calculate()
        this.setState({
          operator: operatorInput,
          firstVal: total + '',
          secondVal: '',
        })
-       setDisplay(total + '')
+       this.setDisplay(total + '')
       } else {
         this.setState({
           operator: operatorInput,
-          firstVal: fixNumberString(display, false),
+          firstVal: this.fixNumberString(this.state.display, false),
         })
       }
     }
     
-    operatorClickHandler = (e) => {
-      const { setOperatorValue } = this
-      const operatorInput = e.target.innerHTML
-      
-      setOperatorValue(operatorInput)
+    operatorClickHandler = (e) => {      
+      this.setOperatorValue(e.target.innerHTML)
     }
     
     allClear = () => {
         this.resetState(true)
     }
     
-    deleteChar = () => {
-      const {
-        firstVal,
-        secondVal,
-        operator,
-      } = this.state
-      
-      if (!operator) {
-        const newVal = firstVal.slice(0, -1)
+    deleteChar = () => {      
+      if (!this.state.operator) {
+        const newVal = this.state.firstVal.slice(0, -1)
         this.setState({
           firstVal: newVal,
           display: newVal ? newVal : '0',
         })
-      } else if (operator && !secondVal) {
+      } else if (this.state.operator && !this.state.secondVal) {
         this.setState({
-          display: firstVal,
+          display: this.state.firstVal,
           operator: '',
         })
       } else {
-        const newVal = secondVal.slice(0, -1)
+        const newVal = this.state.secondVal.slice(0, -1)
         this.setState({
           secondVal: newVal,
           display: newVal ? newVal : '0',
@@ -228,16 +182,12 @@ class Keypad extends React.Component {
     }
     
     fixNumberString = (value, finalize = false) => {
-      // if input has hanging point e.g. '0.'/'1.', add trailing zero
-      // should only run when moving to second value / begin calculation
       if (finalize && value.indexOf('.') === value.length - 1 && value.length > 1) { 
         return value + '0'
       }
-      // if value has zero prefix but not a decimal value, e.g. '01'/'03', remove zero
       if (value.indexOf('0') === 0 && !value.indexOf('0.') === 0) {
         return value.substring(1)
       }
-      // if value is a first point input '.', add zero prefix
       if (value.indexOf('.') === 0 && value.length === 1) {
         return '0.'
       }
@@ -248,20 +198,11 @@ class Keypad extends React.Component {
     }
     
     calculate = () => {
-        const {
-          firstVal,
-        secondVal,
-        operator,
-      } = this.state
-      const {
-        fixNumberString,
-      } = this
-      
-      const vfirstVal = fixNumberString(firstVal, true)
-      const vsecondVal = fixNumberString(secondVal, true)
+      const vfirstVal = this.fixNumberString(this.state.firstVal, true)
+      const vsecondVal = this.fixNumberString(this.state.secondVal, true)
       let total = '0';
       
-      switch (operator) {
+      switch (this.state.operator) {
           case '-' :
             total = parseFloat(vfirstVal) - parseFloat(vsecondVal)
             break;
@@ -280,82 +221,62 @@ class Keypad extends React.Component {
       return total.toLocaleString()
     }
     
-    equalHandler = () => {
-      const {
-        firstVal,
-        secondVal,
-        operator,
-      } = this.state
-      const {
-        setDisplay,
-        resetState,
-        calculate,
-      } = this
-      
-      if (firstVal && secondVal && operator) {
-        let total = calculate()
-        setDisplay(total + '')
-        resetState()
+    equalHandler = () => {      
+      if (this.state.firstVal && this.state.secondVal && this.state.operator) {
+        let total = this.calculate()
+        this.setDisplay(total + '')
+        this.resetState()
       }
     }
 
+    numberButtons = (handler, nums) => {
+      return(
+        <div className="row">
+          <button onClick={handler}>{nums[0]}</button>
+          <button onClick={handler}>{nums[1]}</button>
+          <button onClick={handler}>{nums[2]}</button>
+        </div>
+      );
+    }
+
     render(){
-        const {
-            operatorClickHandler,
-            numberClickHandler,
-            deleteChar,
-            allClear,
-            equalHandler,
-        } = this
         const activeOperator = name => {
             return this.state.operator === name ? 'active' : ''
         }
+        
+        const operatorsArr = ['+', '-', ':', '*']
 
         return(
             <div className="inputs">
                 <div className="column main">
                 <div className="operator">
                     <div className="row">
-                        <button 
-                        className={activeOperator('+')}
-                        onClick={operatorClickHandler}>+</button>
-                        <button 
-                        className={activeOperator('-')}
-                        onClick={operatorClickHandler}>-</button>
-                        <button 
-                        className={activeOperator(':')}
-                        onClick={operatorClickHandler}>:</button>
-                        <button 
-                        className={activeOperator('*')}
-                        onClick={operatorClickHandler}>*</button>
+                        { operatorsArr.map( opr => {
+                            return (
+                              <button 
+                                className={activeOperator(opr)}
+                                onClick={this.operatorClickHandler}>
+                                  {opr}
+                              </button>
+                            )
+                          })
+                        }
                     </div>
                 </div>
                 <div className="numbers">
+                    { this.numberButtons(this.numberClickHandler, [1,2,3])}
+                    { this.numberButtons(this.numberClickHandler, [4,5,6])}
+                    { this.numberButtons(this.numberClickHandler, [7,8,9])}
                     <div className="row">
-                        <button onClick={numberClickHandler}>1</button>
-                        <button onClick={numberClickHandler}>2</button>
-                        <button onClick={numberClickHandler}>3</button>
-                    </div>
-                    <div className="row">
-                        <button onClick={numberClickHandler}>4</button>
-                        <button onClick={numberClickHandler}>5</button>
-                        <button onClick={numberClickHandler}>6</button>
-                    </div>
-                    <div className="row">
-                    <button onClick={numberClickHandler}>7</button>
-                    <button onClick={numberClickHandler}>8</button>
-                    <button onClick={numberClickHandler}>9</button>
-                    </div>
-                    <div className="row">
-                        <button onClick={numberClickHandler}>.</button>
-                        <button onClick={numberClickHandler}>0</button>
-                        <button onClick={deleteChar}>C</button>
+                        <button onClick={this.numberClickHandler}>.</button>
+                        <button onClick={this.numberClickHandler}>0</button>
+                        <button onClick={this.deleteChar}>C</button>
                     </div>
                 </div>
                 </div>
                 <div className="column sides">
-                    <button className="ac" onClick={allClear}>AC</button>
-                    <button className="equal" onClick={equalHandler}>=</button>
+                    <button className="ac" onClick={this.allClear}>AC</button>
+                    <button className="equal" onClick={this.equalHandler}>=</button>
                 </div>
             </div>
         );
